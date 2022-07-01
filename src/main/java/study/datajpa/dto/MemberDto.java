79f6805402903400063a64912d87ca30ecfcfc60
myoungkun_kim <slb09@naver.com>
2022-07-01 16:04:33 +0900

    Study paging and sorting

--- src/main/java/study/datajpa/dto/MemberDto.java
+++ src/main/java/study/datajpa/dto/MemberDto.java
@@ -3,11 +3,19 @@ package study.datajpa.dto;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import study.datajpa.entity.Member;
 
 @Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PUBLIC)
 public class MemberDto {
     private Long id;
     private String username;
     private String teamName;
+
+    public MemberDto(Member member) {
+        this.id = member.getId();
+        this.username = member.getUsername();
+    }
 }
